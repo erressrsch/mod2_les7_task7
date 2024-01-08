adbff95a21a2dd4a7afc451a1a97163cddc9ae98
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:15:18 +0100

    Add project files.

---
+++ mod2_les7_task7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace mod2_les7_task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = [];
+
+            Console.WriteLine("Enter 3 numbers separated by a comma: ");
+            string userinput = Console.ReadLine();
+            string[] words = userinput.Split(',');
+
+            foreach (var word in words)
+            {
+                numbers.Add(int.Parse(word));
+            }
+            numbers.Sort();
+            Console.WriteLine();
+            Console.WriteLine($"{numbers.Max()} is the largest number given");
+
+        }
+    }
+}
